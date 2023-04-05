a1616edc3375687de758aabdb235b54c278a75fe
TangHoang <businesshoangtang@gmail.com>
2023-04-05 11:56:37 +0200

    add navbar for all pages

--- app.js
+++ app.js
@@ -1,29 +1,48 @@
 let date = new Date().toLocaleDateString("de-DE");
+let otherDate = new Date();
+const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+let currentDay = days[otherDate.getDay()];
 let setNumber = 1;
 let exerciseNumber = 1;
-
 let benchPressList = [];
-// test
-
 
 let dateContainer = document.getElementById("date");
 const addSetButton = document.getElementById("add-set");
 const exercisePage = document.getElementsByClassName("log");
 const finishExerciseButton = document.getElementById("finish-exercise");
+const displayCurrentDay = document.getElementById("current-day");
 
 addSetButton.addEventListener("click", addSet);
 finishExerciseButton.addEventListener("click", pushData);
+
+dateContainer.innerHTML = date;
+displayCurrentDay.innerHTML = currentDay;
+
 
 function pushData() {
     let exerciseName = document.getElementById("exercise-input").value;
     let exerciseDataArray = []
     for(let i=1; i<=setNumber; i++){
+        // iterate through sets
         let exerciseData = document.getElementsByClassName(`${i}`);
+        // get values of Nodes
         exerciseDataArray = Array.from(exerciseData).map(t => t.value);
         benchPressList.push(exerciseDataArray);
     }
+    // insert at start of array
     benchPressList.unshift([date,exerciseName]);
     console.log(benchPressList);
+    saveToLocalStorage(benchPressList, date, exerciseName);
+    getFromLocalStorage();
+}
+
+function saveToLocalStorage(benchPressList, date, exerciseName) {
+    let key = date + " " + exerciseName;
+    localStorage.setItem(`${key}`, JSON.stringify(benchPressList));
+}
+
+function getFromLocalStorage(key) {
+    let data = JSON.parse(localStorage.getItem(`${key}`));
 }
 
 function addSet() {
@@ -34,6 +53,7 @@ function addSet() {
     const repsInput = document.createElement("input");
     const okButton = document.createElement("button");
 
+    // add key: "setNumber" for later reference
     kgInput.classList.add(`${setNumber}`);
     repsInput.classList.add(`${setNumber}`);
     newDiv.innerHTML = `${setNumber}`;
@@ -45,4 +65,3 @@ function addSet() {
     exercisePage[0].appendChild(okButton);
 
 }
-dateContainer.innerHTML = date;
