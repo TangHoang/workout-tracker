8fcee26b895474940daf9d24dde7af569724de77
TangHoang <businesshoangtang@gmail.com>
2023-04-06 16:23:45 +0200

    add function that display localStorage on page

--- app.js
+++ app.js
@@ -18,31 +18,38 @@ const deleteExerciseButton = document.getElementById("delete-exercise1");
 const pageIndexDiv = document.getElementById("index_html");
 const pageCalenderDiv = document.getElementById("calender_html");
 const pageLogDiv = document.getElementById("log_html");
+const pageSpecificLogDiv = document.getElementById("specific-log_html");
 const links = document.getElementsByClassName("icon");
 const logsGrid = document.getElementById("logs-grid");
+
+function togglePages(e) {
+    // checks id of link tag
+    let currentPage = e.target.id.substring(5);
+    switch(currentPage) {
+        case "index_html":
+            pageCalenderDiv.style.display = "none";
+            pageIndexDiv.style.display = "flex";
+            pageLogDiv.style.display = "none";
+            pageSpecificLogDiv.style.display = "none";
+            break;
+        case "log_html":
+            pageCalenderDiv.style.display = "none";
+            pageIndexDiv.style.display = "none";
+            pageLogDiv.style.display = "flex";
+            pageSpecificLogDiv.style.display = "none";
+            break;
+        case "calender_html":
+            pageCalenderDiv.style.display = "flex";
+            pageIndexDiv.style.display = "none";
+            pageLogDiv.style.display = "none";
+            pageSpecificLogDiv.style.display = "none";
+    }
+}
 
 // switch pages
 for(let i=0; i<links.length; i++) {
     let self = links[i];
-    self.addEventListener("click", function(e) {
-        let currentPage = e.target.id.substring(5);
-        switch(currentPage) {
-            case "index_html":
-                pageCalenderDiv.style.display = "none";
-                pageIndexDiv.style.display = "flex";
-                pageLogDiv.style.display = "none";
-                break;
-            case "log_html":
-                pageCalenderDiv.style.display = "none";
-                pageIndexDiv.style.display = "none";
-                pageLogDiv.style.display = "flex";
-                break;
-            case "calender_html":
-                pageCalenderDiv.style.display = "flex";
-                pageIndexDiv.style.display = "none";
-                pageLogDiv.style.display = "none";
-        }
-    })
+    self.addEventListener("click", togglePages);
 }
 
 addSetButton.addEventListener("click", addSet);
@@ -55,16 +62,30 @@ displayCurrentDay.innerHTML = currentDay;
 
 function loadLogPage(){
     for(let i=0; i<exerciseList.length; i++){
-        console.log("Guten Morgen");
+        let modifiedString = exerciseList[i].replace((/\s+/g, '')).toLowerCase();
+
         let gridElement = document.createElement("div");
         gridElement.setAttribute("class", "log-grid-element");
+        gridElement.setAttribute("id", `link-${modifiedString}_html`);
         gridElement.innerHTML = `${exerciseList[i]}`;
-        gridElement.addEventListener("click", openExerciseLog);
+        gridElement.addEventListener("click", function(){
+            openExerciseLog(exerciseList[i])
+        });
         logsGrid.appendChild(gridElement);
     }
 }
-function openExerciseLog(e){
-    //TODO
+function openExerciseLog(currentExerciseName){
+    pageCalenderDiv.style.display = "none";
+    pageIndexDiv.style.display = "none";
+    pageLogDiv.style.display = "none";
+    pageSpecificLogDiv.style.display = "flex";
+
+    let allKeys = Object.keys(localStorage);
+    for(let i=0; i<allKeys.length; i++){
+        if(allKeys[i].includes(currentExerciseName)){
+            pageSpecificLogDiv.innerHTML += getFromLocalStorage(allKeys[i]);
+        }
+    }
 }
 function getCurrentExerciseNumber(e){
     let currentExerciseNumber = e.target.id;
@@ -72,14 +93,20 @@ function getCurrentExerciseNumber(e){
     return currentExerciseNumber;
 }
 
+function getCurrentExerciseName(e){
+    let currentExerciseNumber = getCurrentExerciseNumber(e);
+    let exerciseName = document.getElementById(`exercise-${currentExerciseNumber}`).value; //this ID is from the input node
+    return exerciseName;
+}
+
 function changeBackgroundAfterFinish(e){
     const exerciseBody = document.getElementById(`exercise${getCurrentExerciseNumber(e)}`);
     exerciseBody.style.background = "#B9deb7";
 }
-
+// push data to local Storage
 function pushData(e) {
+    let exerciseName = getCurrentExerciseName(e);
     let currentExerciseNumber = getCurrentExerciseNumber(e);
-    let exerciseName = document.getElementById(`exercise-${currentExerciseNumber}`).value; //this ID is from the input node
     let exerciseDataArray = [];
     let exerciseLog = [];
     
@@ -87,21 +114,22 @@ function pushData(e) {
         let exerciseData = document.getElementsByClassName(`${currentExerciseNumber}${i}`);
         exerciseDataArray = Array.from(exerciseData).map(t => t.value);
         exerciseLog.push(exerciseDataArray);
-        console.log(exerciseLog);
     }
     // insert at start of array
     exerciseLog.unshift([date,exerciseName]);
-    saveToLocalStorage(exerciseLog, date, exerciseName);
+    saveToLocalStorage(exerciseLog, exerciseName);
     changeBackgroundAfterFinish(e);
 }
 
-function saveToLocalStorage(benchPressList, date, exerciseName) {
-    let key = date + " " + exerciseName;
-    localStorage.setItem(`${key}`, JSON.stringify(benchPressList));
+function saveToLocalStorage(exerciseLog, exerciseName) {
+    let date = new Date();
+    let key = exerciseName + " " + date;
+    localStorage.setItem(`${key}`, JSON.stringify(exerciseLog));
 }
 
 function getFromLocalStorage(key) {
     let data = JSON.parse(localStorage.getItem(`${key}`));
+    return data;
 }
 
 function deleteExercise(e) {
@@ -159,6 +187,7 @@ function addExercise() {
     const createDatalist = document.createElement("datalist");
     createDatalist.setAttribute("id", "exerciseList");
 
+    // create suggestions for input field
     for(let i=0; i<exerciseList.length; i++) {
         let createOption = document.createElement("option");
         createOption.setAttribute("value", exerciseList[i]);
@@ -201,10 +230,10 @@ function addExercise() {
 
     const createInputKg = document.createElement("input");
     createInputKg.setAttribute("type", "number");
-    createInputDiv.setAttribute("class", `kg-input ${exerciseNumber}${setNumber}`);
+    createInputKg.setAttribute("class", `kg-input ${exerciseNumber}${setNumber}`);
     const createInputReps = document.createElement("input");
     createInputReps.setAttribute("type", "number");
-    createInputDiv.setAttribute("class", `reps-input ${exerciseNumber}${setNumber}`);
+    createInputReps.setAttribute("class", `reps-input ${exerciseNumber}${setNumber}`);
 
     const createDeleteSetButton = document.createElement("button");
     createDeleteSetButton.innerHTML = "X";
